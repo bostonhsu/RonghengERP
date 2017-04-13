44c89f404c61b933e6f00b7e6d3eb5f24f2762d1
徐智勇 <bostonhsu@outlook.com>
2017-04-13 10:17:13 +0800

    更新了高级排产的界面。

--- WangNeedSearch/WangNeedSearch/Main.Designer.cs
+++ WangNeedSearch/WangNeedSearch/Main.Designer.cs
@@ -32,14 +32,16 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.lblCount = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSearchNumbers
             // 
             this.btnSearchNumbers.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.btnSearchNumbers.Location = new System.Drawing.Point(12, 63);
+            this.btnSearchNumbers.Location = new System.Drawing.Point(24, 126);
+            this.btnSearchNumbers.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnSearchNumbers.Name = "btnSearchNumbers";
-            this.btnSearchNumbers.Size = new System.Drawing.Size(110, 23);
+            this.btnSearchNumbers.Size = new System.Drawing.Size(220, 46);
             this.btnSearchNumbers.TabIndex = 0;
             this.btnSearchNumbers.Text = "查询数量";
             this.btnSearchNumbers.UseVisualStyleBackColor = false;
@@ -47,42 +49,58 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(128, 6);
+            this.panel1.Location = new System.Drawing.Point(256, 12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(450, 450);
+            this.panel1.Size = new System.Drawing.Size(900, 900);
             this.panel1.TabIndex = 2;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.PaleGoldenrod;
-            this.button1.Location = new System.Drawing.Point(12, 110);
+            this.button1.Location = new System.Drawing.Point(24, 222);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.Size = new System.Drawing.Size(220, 46);
             this.button1.TabIndex = 3;
-            this.button1.Text = "其他功能";
+            this.button1.Text = "高级排产";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // lblCount
             // 
             this.lblCount.AutoSize = true;
-            this.lblCount.Location = new System.Drawing.Point(13, 248);
+            this.lblCount.Location = new System.Drawing.Point(26, 496);
+            this.lblCount.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblCount.Name = "lblCount";
-            this.lblCount.Size = new System.Drawing.Size(41, 12);
+            this.lblCount.Size = new System.Drawing.Size(82, 24);
             this.lblCount.TabIndex = 4;
             this.lblCount.Text = "label1";
             this.lblCount.Visible = false;
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GreenYellow;
+            this.button2.Location = new System.Drawing.Point(24, 318);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "其他功能";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.ClientSize = new System.Drawing.Size(1168, 922);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.lblCount);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnSearchNumbers);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.Name = "main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -100,6 +118,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button button2;
     }
 }
 
