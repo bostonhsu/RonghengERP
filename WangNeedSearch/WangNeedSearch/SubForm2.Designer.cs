ca920dfd6791811c3c223b34d7474a6abc015a40
Boston Hsu <bostonhsu@outlook.com>
2017-04-13 14:11:32 +0800

    把高级排产界面的ID重新设置了一下。

--- WangNeedSearch/WangNeedSearch/SubForm2.Designer.cs
+++ WangNeedSearch/WangNeedSearch/SubForm2.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtOrderNumber = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.trvOrderDetail = new System.Windows.Forms.TreeView();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtDetailCount = new System.Windows.Forms.TextBox();
+            this.lstFactory = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -42,96 +42,104 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "订单号：";
             // 
-            // textBox1
+            // txtOrderNumber
             // 
-            this.textBox1.Location = new System.Drawing.Point(143, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(238, 35);
-            this.textBox1.TabIndex = 1;
+            this.txtOrderNumber.Location = new System.Drawing.Point(72, 14);
+            this.txtOrderNumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtOrderNumber.Name = "txtOrderNumber";
+            this.txtOrderNumber.Size = new System.Drawing.Size(121, 21);
+            this.txtOrderNumber.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(387, 28);
+            this.button1.Location = new System.Drawing.Point(194, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 40);
+            this.button1.Size = new System.Drawing.Size(66, 20);
             this.button1.TabIndex = 2;
             this.button1.Text = "查找订单";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // treeView1
+            // trvOrderDetail
             // 
-            this.treeView1.Location = new System.Drawing.Point(35, 87);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(346, 535);
-            this.treeView1.TabIndex = 3;
+            this.trvOrderDetail.Location = new System.Drawing.Point(18, 44);
+            this.trvOrderDetail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trvOrderDetail.Name = "trvOrderDetail";
+            this.trvOrderDetail.Size = new System.Drawing.Size(175, 270);
+            this.trvOrderDetail.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(406, 118);
+            this.label2.Location = new System.Drawing.Point(203, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 24);
+            this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 4;
             this.label2.Text = "下单数量：";
             // 
-            // textBox2
+            // txtDetailCount
             // 
-            this.textBox2.Location = new System.Drawing.Point(542, 115);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(146, 35);
-            this.textBox2.TabIndex = 5;
+            this.txtDetailCount.Location = new System.Drawing.Point(271, 58);
+            this.txtDetailCount.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDetailCount.Name = "txtDetailCount";
+            this.txtDetailCount.Size = new System.Drawing.Size(75, 21);
+            this.txtDetailCount.TabIndex = 5;
             // 
-            // listBox1
+            // lstFactory
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 24;
-            this.listBox1.Location = new System.Drawing.Point(405, 236);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(307, 388);
-            this.listBox1.TabIndex = 6;
+            this.lstFactory.FormattingEnabled = true;
+            this.lstFactory.ItemHeight = 12;
+            this.lstFactory.Location = new System.Drawing.Point(202, 118);
+            this.lstFactory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstFactory.Name = "lstFactory";
+            this.lstFactory.Size = new System.Drawing.Size(156, 196);
+            this.lstFactory.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(406, 192);
+            this.label3.Location = new System.Drawing.Point(203, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 24);
+            this.label3.Size = new System.Drawing.Size(65, 12);
             this.label3.TabIndex = 7;
             this.label3.Text = "生成工厂：";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(743, 587);
+            this.button2.Location = new System.Drawing.Point(372, 294);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 40);
+            this.button2.Size = new System.Drawing.Size(66, 20);
             this.button2.TabIndex = 8;
             this.button2.Text = "下单";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // SubForm2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGoldenrod;
-            this.ClientSize = new System.Drawing.Size(900, 900);
+            this.ClientSize = new System.Drawing.Size(450, 390);
             this.ControlBox = false;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lstFactory);
+            this.Controls.Add(this.txtDetailCount);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.trvOrderDetail);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtOrderNumber);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "SubForm2";
             this.ShowIcon = false;
             this.Text = "SubForm2";
@@ -144,12 +152,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtOrderNumber;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView trvOrderDetail;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtDetailCount;
+        private System.Windows.Forms.ListBox lstFactory;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
     }
